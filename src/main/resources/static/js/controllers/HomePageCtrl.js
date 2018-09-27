6741dae1beca73732c35ba49a5665ea9db987ff6
tbindu <tbindu@thoughtworks.com>
2018-09-27 15:53:24 +0530

    Swathi,Bindu | BAH-648 | upping karma to v0.13 and preparing for next development iteration

--- src/main/resources/static/js/controllers/HomePageCtrl.js
+++ src/main/resources/static/js/controllers/HomePageCtrl.js
@@ -3,7 +3,6 @@ var atomFeedConsoleApp = angular.module('atomFeedConsole');
 atomFeedConsoleApp.controller('HomePageCtrl',['$scope','HomePageService',
  function($scope, HomePageService){
     HomePageService.getAppDetails().then(function(response){
-        console.log(JSON.stringify(response.data))
         $scope.results = response.data;
     });
 
